0cbb0b27c1195781496f1a0397359de921aa0797
hardikk04 <hardik04062003@gmail.com>
2024-02-21 04:03:46 +0530

    page2 animation update

--- script.js
+++ script.js
@@ -48,12 +48,16 @@ function loaderAnimation() {
     delay: 0.5,
   });
 
-  loaderTl.to(".loader", {
-    top: "-100vh",
-    ease: "power4.inOut",
-    duration: 1,
-    delay: 0.5,
-  });
+  loaderTl.to(
+    ".loader",
+    {
+      opacity: 0,
+      ease: "power4.inOut",
+      duration: 1,
+      delay: 0.5,
+    },
+    "same"
+  );
 
   loaderTl.from("nav", {
     y: -30,
@@ -246,27 +250,27 @@ function textFlyoutAnimation() {
     },
   });
 
-  tl.to(
+  tl.from(
     ".p",
     {
-      x: -820,
-      y: -200,
+      x: -520,
+      y: 100,
       scale: 5,
-      rotate: -180,
+      rotate: -320,
     },
     "same"
   )
-    .to(
+    .from(
       ".h",
       {
-        x: -950,
-        y: 100,
-        scale: 6,
-        rotate: 180,
+        x: -250,
+        y: -700,
+        scale: 4,
+        rotate: 220,
       },
       "same"
     )
-    .to(
+    .from(
       ".i",
       {
         y: -500,
@@ -277,16 +281,17 @@ function textFlyoutAnimation() {
       "same"
     )
 
-    .to(
+    .from(
       ".l",
       {
-        y: -800,
+        y: -650,
+        x: 200,
         rotate: 260,
         scale: 6,
       },
       "same"
     )
-    .to(
+    .from(
       ".i2",
       {
         y: -500,
@@ -297,31 +302,31 @@ function textFlyoutAnimation() {
       "same"
     )
 
-    .to(
+    .from(
       ".p2",
       {
-        y: -700,
-        x: 900,
+        y: -200,
+        x: 400,
         rotate: 300,
-        scale: 8,
+        scale: 2,
       },
       "same"
     )
-    .to(
+    .from(
       ".p3",
       {
-        y: -400,
-        x: 800,
+        y: 50,
+        x: -200,
         rotate: 300,
         scale: 5,
       },
       "same"
     )
-    .to(
+    .from(
       ".e",
       {
-        x: 850,
-        y: 100,
+        x: 520,
+        y: 200,
         scale: 5,
         rotate: 180,
       },
@@ -342,8 +347,393 @@ gsap.to(".svg-overlay", {
   },
 });
 
-clutterAnimation(".page2-title-box > h1");
-gsap.from(".page2-title-box > h1>span", {
+function page2Animation() {
+  clutterAnimation(".page2-title > h1");
+  gsap.from(".page2-title > h1>span", {
+    y: 200,
+    opacity: 0,
+    rotate: 180,
+    stagger: {
+      amount: 2,
+      from: "random",
+    },
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".page2-title>h1",
+      start: "top 60%",
+      end: "top 10%",
+      scrub: 1,
+      // markers: true,
+    },
+  });
+
+  const page2PartsTl = gsap.timeline({
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".page2",
+      start: "top 0%",
+      end: "top -1000%",
+      pin: true,
+      scrub: 1,
+      // markers: true,
+    },
+  });
+
+  gsap.from(".page2-right,.page2-rotation", {
+    opacity: 0,
+    scrollTrigger: {
+      scroller: "body",
+      trigger: ".page2",
+      start: "top 0%",
+      end: "top -100%",
+      scrub: 1,
+      // markers: true,
+    },
+  });
+
+  page2PartsTl.to(
+    ".parts-counter",
+    {
+      onUpdate: () => {
+        document.querySelector(".parts-counter").textContent = "01";
+        const allNavigation = document.querySelectorAll(
+          ".page2-navigation-circle"
+        );
+        allNavigation.forEach((item, index) => {
+          if (index === 0) {
+            gsap.to(item, {
+              backgroundColor: "#000",
+            });
+          } else {
+            gsap.to(item, {
+              backgroundColor: "#fff",
+            });
+          }
+        });
+      },
+    },
+    "same"
+  );
+
+  page2PartsTl.to(
+    ".page2",
+    {
+      backgroundColor: "#DB4D1A",
+    },
+    "same"
+  );
+
+  page2PartsTl.to(
+    ".page2-part1",
+    {
+      top: "-100%",
+    },
+    "same"
+  );
+
+  page2PartsTl.to(
+    ".part1-text",
+    {
+      left: "-100%",
+    },
+    "same"
+  );
+
+  page2PartsTl.to(
+    ".parts-counter",
+    {
+      onUpdate: () => {
+        document.querySelector(".parts-counter").textContent = "02";
+        const allNavigation = document.querySelectorAll(
+          ".page2-navigation-circle"
+        );
+        allNavigation.forEach((item, index) => {
+          if (index === 1) {
+            gsap.to(item, {
+              backgroundColor: "#000",
+            });
+          } else {
+            gsap.to(item, {
+              backgroundColor: "#fff",
+            });
+          }
+        });
+      },
+    },
+    "same1"
+  );
+
+  page2PartsTl.to(
+    ".page2",
+    {
+      backgroundColor: "#B3B3B3",
+    },
+    "same1"
+  );
+
+  page2PartsTl.to(
+    ".page2-part2",
+    {
+      top: "-100%",
+    },
+    "same1"
+  );
+
+  page2PartsTl.to(
+    ".part2-text",
+    {
+      left: "-100%",
+    },
+    "same1"
+  );
+
+  page2PartsTl.to(
+    ".parts-counter",
+    {
+      onUpdate: () => {
+        document.querySelector(".parts-counter").textContent = "03";
+        const allNavigation = document.querySelectorAll(
+          ".page2-navigation-circle"
+        );
+        allNavigation.forEach((item, index) => {
+          if (index === 2) {
+            gsap.to(item, {
+              backgroundColor: "#000",
+            });
+          } else {
+            gsap.to(item, {
+              backgroundColor: "#fff",
+            });
+          }
+        });
+      },
+    },
+    "same2"
+  );
+
+  page2PartsTl.to(
+    ".page2",
+    {
+      backgroundColor: "#3E8DCE",
+    },
+    "same2"
+  );
+
+  page2PartsTl.to(
+    ".page2-part3",
+    {
+      top: "-100%",
+    },
+    "same2"
+  );
+
+  page2PartsTl.to(
+    ".part3-text",
+    {
+      left: "-100%",
+    },
+    "same2"
+  );
+
+  page2PartsTl.to(
+    ".parts-counter",
+    {
+      onUpdate: () => {
+        document.querySelector(".parts-counter").textContent = "04";
+        const allNavigation = document.querySelectorAll(
+          ".page2-navigation-circle"
+        );
+        allNavigation.forEach((item, index) => {
+          if (index === 3) {
+            gsap.to(item, {
+              backgroundColor: "#000",
+            });
+          } else {
+            gsap.to(item, {
+              backgroundColor: "#fff",
+            });
+          }
+        });
+      },
+    },
+    "same3"
+  );
+
+  page2PartsTl.to(
+    ".page2",
+    {
+      backgroundColor: "#A031FF",
+    },
+    "same3"
+  );
+
+  page2PartsTl.to(
+    ".page2-part4",
+    {
+      top: "-100%",
+    },
+    "same3"
+  );
+
+  page2PartsTl.to(
+    ".part4-text",
+    {
+      left: "-100%",
+    },
+    "same3"
+  );
+
+  page2PartsTl.to(
+    ".parts-counter",
+    {
+      onUpdate: () => {
+        document.querySelector(".parts-counter").textContent = "05";
+        const allNavigation = document.querySelectorAll(
+          ".page2-navigation-circle"
+        );
+        allNavigation.forEach((item, index) => {
+          if (index === 4) {
+            gsap.to(item, {
+              backgroundColor: "#000",
+            });
+          } else {
+            gsap.to(item, {
+              backgroundColor: "#fff",
+            });
+          }
+        });
+      },
+    },
+    "same4"
+  );
+
+  page2PartsTl.to(
+    ".page2",
+    {
+      backgroundColor: "#3B7977",
+    },
+    "same4"
+  );
+
+  page2PartsTl.to(
+    ".page2-part5",
+    {
+      top: "-100%",
+    },
+    "same4"
+  );
+
+  page2PartsTl.to(
+    ".part5-text",
+    {
+      left: "-100%",
+    },
+    "same4"
+  );
+
+  page2PartsTl.to(
+    ".parts-counter",
+    {
+      onUpdate: () => {
+        document.querySelector(".parts-counter").textContent = "06";
+        const allNavigation = document.querySelectorAll(
+          ".page2-navigation-circle"
+        );
+        allNavigation.forEach((item, index) => {
+          if (index === 5) {
+            gsap.to(item, {
+              backgroundColor: "#000",
+            });
+          } else {
+            gsap.to(item, {
+              backgroundColor: "#fff",
+            });
+          }
+        });
+      },
+    },
+    "same5"
+  );
+
+  page2PartsTl.to(
+    ".page2",
+    {
+      backgroundColor: "#1477FB",
+    },
+    "same5"
+  );
+
+  page2PartsTl.to(
+    ".page2-part6",
+    {
+      top: "-100%",
+    },
+    "same5"
+  );
+
+  page2PartsTl.to(
+    ".part6-text",
+    {
+      left: "-100%",
+    },
+    "same5"
+  );
+
+  page2PartsTl.to(
+    ".parts-counter",
+    {
+      onUpdate: () => {
+        document.querySelector(".parts-counter").textContent = "07";
+        const allNavigation = document.querySelectorAll(
+          ".page2-navigation-circle"
+        );
+        allNavigation.forEach((item, index) => {
+          if (index === 6) {
+            gsap.to(item, {
+              backgroundColor: "#000",
+            });
+          } else {
+            gsap.to(item, {
+              backgroundColor: "#fff",
+            });
+          }
+        });
+      },
+    },
+    "same6"
+  );
+
+  page2PartsTl.to(
+    ".page2",
+    {
+      backgroundColor: "#AE928D",
+    },
+    "same6"
+  );
+
+  page2PartsTl.to(
+    ".page2-part7",
+    {
+      top: "-100%",
+    },
+    "same6"
+  );
+
+  page2PartsTl.to(
+    ".part7-text",
+    {
+      left: "-100%",
+    },
+    "same6"
+  );
+  page2PartsTl.to(".page2", {
+    backgroundColor: "#EBE4DF",
+  });
+}
+page2Animation();
+
+clutterAnimation(".page3-title-box > h1");
+gsap.from(".page3-title-box > h1>span", {
   y: 200,
   opacity: 0,
   rotate: 180,
@@ -353,7 +743,7 @@ gsap.from(".page2-title-box > h1>span", {
   },
   scrollTrigger: {
     scroller: "body",
-    trigger: ".page2-title-box",
+    trigger: ".page3-title-box",
     start: "top 75%",
     end: "top 40%",
     scrub: 1,
@@ -361,345 +751,49 @@ gsap.from(".page2-title-box > h1>span", {
   },
 });
 
-function page2Part1Animation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part1",
-      start: "top 40%",
-      end: "top 0%",
-      scrub: 1,
-      // markers: true,
-    },
+function page3MouseFollowerAnimation() {
+  const page3TextBox = document.querySelector(".page3-box");
+
+  // Select the circle element
+  const circleElement = document.querySelector(".page3-mousefollower");
+
+  page3TextBox.addEventListener("mousemove", (dets) => {
+    gsap.to(".page3-mousefollower", {
+      opacity: 1,
+      left: dets.x,
+      top: dets.y,
+      scale: 1,
+    });
   });
-  tl.to(
-    ".main",
-    {
-      backgroundColor: "#DB4D1A",
-    },
-    "same"
-  );
-  tl.to(
-    ".page2-title-box > h1",
-    {
-      color: "#fff",
-    },
-    "same"
-  );
 
-  tl.from(
-    ".page2-part1-right>h3",
-    {
+  page3TextBox.addEventListener("mouseleave", (e) => {
+    gsap.to(".page3-mousefollower", {
       opacity: 0,
-    },
-    "same"
-  );
-  tl.from(
-    ".page2-part1-circle",
-    {
-      y: 600,
-    },
-    "same"
-  );
-
-  tl.from(".page2-part1 > h1", {
-    left: "100%",
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part1",
-      start: "top 40%",
-      end: "top -40%",
-      scrub: 1,
-      // markers: true,
-    },
+      scale: 0,
+    });
   });
 }
-page2Part1Animation();
 
-function page2Part2Animation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part2",
-      start: "top 40%",
-      end: "top 0%",
-      scrub: 1,
-      // markers: true,
-    },
+page3MouseFollowerAnimation();
+
+const mousefollowerImgArray = ["./Imgs/coco-cola.png", ""];
+const page3Img = document.querySelector(".page3-mousefollower>img");
+
+const page3AllElem = document.querySelectorAll(".page3-elem");
+page3AllElem.forEach((elem, index) => {
+  elem.addEventListener("mouseenter", () => {
+    gsap.to(page3Img, {
+      // Example: Fade out the image
+      onComplete: function () {
+        // Change the src attribute after the animation is complete
+        page3Img.src = mousefollowerImgArray[index];
+
+        // Use GSAP to fade in the new image
+        gsap.to(page3Img, {
+          duration: 0.5,
+          opacity: 1,
+        });
+      },
+    });
   });
-  tl.to(
-    ".main",
-    {
-      backgroundColor: "#B3B3B3",
-    },
-    "same"
-  );
-
-  tl.from(
-    ".page2-part2-right>h3",
-    {
-      opacity: 0,
-    },
-    "same"
-  );
-  tl.from(
-    ".page2-part2-circle",
-    {
-      y: 600,
-    },
-    "same"
-  );
-
-  tl.from(".page2-part2 > h1", {
-    left: "100%",
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part2",
-      start: "top 40%",
-      end: "top -40%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-}
-page2Part2Animation();
-
-function page2Part3Animation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part3",
-      start: "top 40%",
-      end: "top 0%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-  tl.to(
-    ".main",
-    {
-      backgroundColor: "#3E8DCE",
-    },
-    "same"
-  );
-
-  tl.from(
-    ".page2-part3-right>h3",
-    {
-      opacity: 0,
-    },
-    "same"
-  );
-  tl.from(
-    ".page2-part3-circle",
-    {
-      y: 600,
-    },
-    "same"
-  );
-
-  tl.from(".page2-part3 > h1", {
-    left: "100%",
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part3",
-      start: "top 40%",
-      end: "top -40%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-}
-page2Part3Animation();
-
-function page2Part4Animation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part4",
-      start: "top 40%",
-      end: "top 0%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-  tl.to(
-    ".main",
-    {
-      backgroundColor: "#A031FF",
-    },
-    "same"
-  );
-
-  tl.from(
-    ".page2-part4-right>h3",
-    {
-      opacity: 0,
-    },
-    "same"
-  );
-  tl.from(
-    ".page2-part4-circle",
-    {
-      y: 600,
-    },
-    "same"
-  );
-
-  tl.from(".page2-part4 > h1", {
-    left: "100%",
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part4",
-      start: "top 40%",
-      end: "top -40%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-}
-page2Part4Animation();
-
-function page2Part5Animation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part5",
-      start: "top 40%",
-      end: "top 0%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-  tl.to(
-    ".main",
-    {
-      backgroundColor: "#3B7977",
-    },
-    "same"
-  );
-
-  tl.from(
-    ".page2-part5-right>h3",
-    {
-      opacity: 0,
-    },
-    "same"
-  );
-  tl.from(
-    ".page2-part5-circle",
-    {
-      y: 600,
-    },
-    "same"
-  );
-
-  tl.from(".page2-part5 > h1", {
-    left: "100%",
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part5",
-      start: "top 40%",
-      end: "top -40%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-}
-page2Part5Animation();
-
-function page2Part6Animation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part6",
-      start: "top 40%",
-      end: "top 0%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-  tl.to(
-    ".main",
-    {
-      backgroundColor: "#1477FB",
-    },
-    "same"
-  );
-
-  tl.from(
-    ".page2-part6-right>h3",
-    {
-      opacity: 0,
-    },
-    "same"
-  );
-  tl.from(
-    ".page2-part6-circle",
-    {
-      y: 600,
-    },
-    "same"
-  );
-
-  tl.from(".page2-part6 > h1", {
-    left: "100%",
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part6",
-      start: "top 40%",
-      end: "top -40%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-}
-page2Part6Animation();
-
-function page2Part7Animation() {
-  const tl = gsap.timeline({
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part7",
-      start: "top 40%",
-      end: "top 0%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-  tl.to(
-    ".main",
-    {
-      backgroundColor: "#AE928D",
-    },
-    "same"
-  );
-
-  tl.from(
-    ".page2-part7-right>h3",
-    {
-      opacity: 0,
-    },
-    "same"
-  );
-  tl.from(
-    ".page2-part7-circle",
-    {
-      y: 600,
-    },
-    "same"
-  );
-
-  tl.from(".page2-part7 > h1", {
-    left: "100%",
-    scrollTrigger: {
-      scroller: "body",
-      trigger: ".page2-part7",
-      start: "top 40%",
-      end: "top -40%",
-      scrub: 1,
-      // markers: true,
-    },
-  });
-}
-page2Part7Animation();
+});
